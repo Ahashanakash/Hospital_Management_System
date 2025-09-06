2d1b26eb817cf58a163016e67e76c1040658456f
Ahashan_Akash <akash24534@gmail.com>
2025-09-06 11:29:20 +0600

    first name and last name of doctor added

--- doctor/serializers.py
+++ doctor/serializers.py
@@ -2,13 +2,25 @@ from rest_framework import serializers
 from . import models
 
 class DoctorSerializer(serializers.ModelSerializer):
-    user = serializers.StringRelatedField(many=False)
+    first_name = serializers.SerializerMethodField()
+    last_name = serializers.SerializerMethodField()
+    # email = serializers.SerializerMethodField()
     designation = serializers.StringRelatedField(many=True)
     specialization = serializers.StringRelatedField(many=True)
     available_time = serializers.StringRelatedField(many=True)
+    
+    def get_first_name(self, obj):
+        return obj.user.first_name if obj.user else None
+    
+    def get_last_name(self, obj):
+        return obj.user.last_name if obj.user else None
+    
+    # def get_email(self, obj):
+    #     return obj.user.email if obj.user else None
+    
     class Meta:
         model = models.Doctor
-        fields = '__all__'
+        fields = ['id', 'first_name', 'last_name', 'image', 'designation', 'specialization', 'available_time', 'fee', 'meet_link']
 class SpecializationSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Specialization
